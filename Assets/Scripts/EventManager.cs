ba742d8b8a0f933c59d440a4f6441f53721a1eff
Fogrexon <katsu15873@gmail.com>
2020-04-27 16:57:59 +0900

    initial commit

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    float timeScale = 1.0f;
+
+    float approachRate = 0.02f;
+    public Transform player;
+    public Transform goal;
+
+    bool goaled = false;
+
+    WorldStatus ws;
+    
+    void Start()
+    {
+        ws = GetComponent<WorldStatus>();
+    }
+
+    void approaching() {
+        Vector3 pos = player.position + (goal.position - player.position) * approachRate;
+        player.position = pos;
+        timeScale = 0.3f;
+        if((goal.position - player.position).magnitude < 0.03) ws.goaled = true;
+    }
+
+    public void changeColor(int index){
+        ws.worldColorIndex = index;
+        ws.targetColor = ws.worldColors[index];
+        ws.worldColorChange = 120;
+    }
+
+    void FixedUpdate()
+    {
+        if(ws.goaled) return;
+        if(ws.goalApproach) approaching();
+        ws.time += timeScale / 60.0f;
+
+        if(ws.worldColorChange <= 0) return;
+        ws.worldColorChange --;
+        ws.worldColor += (ws.targetColor - ws.worldColor) * 0.04f;
+    }
+}
---
+++ Assets/Scripts/WorldStatus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldStatus : MonoBehaviour
+{
+    public Color[] worldColors;
+    public Color worldColor;
+    public int worldColorIndex= 0;
+    public Color targetColor;
+    public int worldColorChange = 2;
+    public bool goalApproach = false;
+    public bool goaled = false;
+
+    public float time = 0.0f;
+
+    public void Start(){
+        worldColor = worldColors[worldColorIndex];
+    }
+    
+}
